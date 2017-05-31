ff64dd6a773e5ced908b4357e5730fbdf6e73947
danecjensen <danecjensen@gmail.com>
2017-05-31 11:27:02 -0500

    adding filename to block

--- app/models/csv_db.rb
+++ app/models/csv_db.rb
@@ -3,11 +3,12 @@ class CsvDb
   class << self
     def convert_save(target_model, csv_data, &block)
       csv_file = csv_data.read
+      csv_filename = csv_data.original_filename
       CSV.parse(csv_file, :headers => true, header_converters: :symbol ) do |row|
         data = row.to_hash
         if data.present?
           if (block_given?)
-             block.call(target_model, data)
+             block.call(target_model, data, csv_filename)
            else
              target_model.create!(data)
            end
